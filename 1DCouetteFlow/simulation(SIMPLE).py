86587776bcb975eb0bb9021629f434470c730d61
chenhan <chenhan200006@shu.edu.cn>
2024-09-11 10:08:29 +0800

    modified 1DCouetteFlow/simulation(SIMPLE).py

--- 1DCouetteFlow/simulation(SIMPLE).py
+++ 1DCouetteFlow/simulation(SIMPLE).py
@@ -11,21 +11,30 @@ import sys
 # Momentum:             d(ρu1)/dt + d(ρu1^2)/dx + d(ρu1u2)/dy = -d(p)/dx + μ(d^2(u1)/dx^2 + d^2(u1)/dy^2)
 #                       d(ρu2)/dt + d(ρu1u2)/dx + d(ρu2^2)/dy = -d(p)/dy + μ(d^2(u2)/dx^2 + d^2(u2)/dy^2)
 
-def BCSet(u1 ,u2, pp, U):
+def velBCSet(rho, u1, u2, U):
     # upper BC
     u1[-1,:] = U
     u2[-1,:] = 0
-    pp[-1,:] = 0
     # lower BC
     u1[0,:] = 0
     u2[0,:] = 0
-    pp[0,:] = 0
     # inlet BC
     u2[:,0] = 0
+
+    rhoU1 = u1[:,:]*rho[1:-1,1:]
+    rhoU2 = u2[:,:]*rho[:,1:]
+    return u1, u2, rhoU1, rhoU2
+
+def presBCSet(pp):
+    # upper
+    pp[-1,:] = 0
+    # lower
+    pp[0,:] = 0
+    # inlet
     pp[:,0] = 0
-    # outlet BC
+    # outlet
     pp[:,-1] = 0
-    return u1, u2, pp
+    return pp
 
 def init():
 
@@ -46,40 +55,45 @@ def init():
         return x, y, U, mu, nt, Courant
 
     def originalVariablesInit(x, y):
-        # rho = np.zeros((len(y),len(x)),dtype=float)
+        rho = np.ones((len(y)+2,len(x)+1),dtype=float)
         p = np.zeros((len(y),len(x)),dtype=float)
         u1 = np.zeros((len(y),len(x)+1),dtype=float)    #rho*u1
         u2 = np.zeros((len(y)+2,len(x)+1),dtype=float)  #rho*u2
-        return u1, u2, p
+        return rho, u1, u2, p
 
-    def flowFieldInit(u1, u2, pstar):
+    def flowFieldInit(rho, u1, u2, pstar):
         u1[:,:] = 0
         u2[:,:] = 0
         pstar[:,:] = 0
         pp = np.zeros_like(pstar)
         u1[14,4] = 0.5 #ft/s
-        return u1, u2, pstar, pp
+
+        rhoU1 = u1[:,:]*rho[1:-1,1:]
+        rhoU2 = u2[:,:]*rho[:,1:]
+
+        return rhoU1, rhoU2, u1, u2, pstar, pp
 
     x ,y ,U, mu, nt, Courant = parameterInput()
-    u1, u2, pstar = originalVariablesInit(x, y)
-    u1, u2, pstar, pp = flowFieldInit(u1, u2, pstar)
-    u1, u2, pp = BCSet(u1 ,u2, pp, U)
+    rho, u1, u2, ps = originalVariablesInit(x, y)
+    rhoU1, rhoU2, u1, u2, ps, pp = flowFieldInit(rho, u1, u2, ps)
+    u1, u2, rhoU1, rhoU2 = velBCSet(rho, u1 ,u2, U)
+    pp = presBCSet(pp)
 
-    return x, y, u1, u2, pstar, pp, nt, mu, Courant
+    return x, y, u1, u2, rhoU1, rhoU2, ps, pp, nt, mu, Courant
 
-def tscheme(x, y, rhoU1, rhoU2, ps, pp, mu, Courant):
+def tscheme(x, y, u1, u2, rhoU1, rhoU2, ps, pp, mu, Courant):
 
-    def semiStep(dx, dy, dt, mu):
+    def semiStep(u1, u2, rhoU1, rhoU2, dx, dy, dt, mu):
         # calculate velocity with divergence
         visTerm1 = mu*( (u1[i+1,j]-2*u1[i,j]+u1[i-1,j])/dx**2 + (u1[i,j+1]-2*u1[i,j]+u1[i,j-1])/dy**2 )
         presTerm1 = (p[i+1,j] - p[i,j])/dx
-        convTerm1 = -((rho[i+1,j]*u1[i+1,j]**2        - rho[i,j]*u1[i,j]**2)/2/dx +
-                      (rho[i,j+1]*u1[i,j+1]*u2[i,j+1] - rho[i,j]*u1[i,j]*u2[i,j])/2/dy)
+        convTerm1 = -((rhoU1[i+1,j]*u1[i+1,j] - rhoU1[i,j]*u1[i,j])/2/dx +
+                      (rhoU1[i,j+1]*u2[i,j+1] - rhoU1[i,j]*u2[i,j])/2/dy)
 
         visTerm2 = mu*( (u2[i+1,j]-2*u2[i,j]+u2[i-1,j])/dx**2 + (u2[i,j+1]-2*u2[i,j]+u2[i,j-1])/dy**2 )
         presTerm2 = (p[i,j+1] - p[i,j])/dy
-        convTerm2 = -((rho[i+1,j]*u1[i+1,j]*u2[i+1,j] - rho[i,j]*u1[i,j]+u2[i,j])/2/dx +
-                      (rho[i,j+1]*u2[i,j+1]**2        - rho[i,j]*u2[i,j]**2)/2/dy)
+        convTerm2 = -((rhoU2[i+1,j]*u1[i+1,j] - rhoU1[i,j]*u2[i,j])/2/dx +
+                      (rhoU2[i,j+1]*u2[i,j+1] - rhoU2[i,j]*u2[i,j])/2/dy)
 
         A = convTerm1 + visTerm1 - presTerm1
         B = convTerm2 + visTerm2 - presTerm2
@@ -88,35 +102,27 @@ def tscheme(x, y, rhoU1, rhoU2, ps, pp, mu, Courant):
 
         return  newRhoU1, newRhoU2
 
-    def pCorrect(rhoU1, rhoU2, ps, dx, dy, dt):
-
-        pp = pIncrement(rhoU1, rhoU2, ps, dx, dy, dt)
-        newP = ps + pp
-
-        return newP
-
-    def pIncrement(rhoU1, rhoU2, ps, dx, dy, dt):
-
+    def pCorrect(rhoU1, rhoU2, pp, dx, dy, dt):
         #   a*ps[i,j] + b*(ps[i+1,j] + ps[i-1,j]) + c*(ps[i,j+1] + ps[i,j-1]) + d = 0
         a = 2*(1/dx**2 + 1/dy**2)*dt
         b = -dt/dx**2
         c = -dt/dy**2
-        d = (rhoU1[i+1,j] - rhoU1[i,j])/dx + (rhoU2[i,j+1] - rhoU2[i,j])/dy
-
-        # solve Poisson equation
-        for i in range(Lx):
-            for j in range(Ly):
-                ps[i,j] = (b*(ps[i+1,j] + ps[i-1,j]) + c*(ps[i,j+1] + ps[i,j-1]) + d)/a
-
-        return ps
+        # solve Poisson equation using Relaxation Method
+        for i in range(1,len(pp[:,0])-1):
+            for j in range(1,len(pp[0,:])-1):
+                d = (rhoU1[i+1,j] - rhoU1[i,j])/dx + (rhoU2[i,j+1] - rhoU2[i,j])/dy
+                pp[i,j] = (b*(pp[i+1,j] + pp[i-1,j]) + c*(pp[i,j+1] + pp[i,j-1]) + d)/a
+        return pp
 
     dx = x[1] - x[0]
     dy = y[1] - y[0]
     dt = Courant*np.sqrt(dx**2 + dy**2)
 
-    newRhoU1, newRhoU2 = semiStep(dx, dy, dt, mu)
+    newRhoU1, newRhoU2 = semiStep(u1, u2, rhoU1, rhoU2, dx, dy, dt, mu)
 
-    newP = pCorrect(newRhoU1, newRhoU2, ps, dx, dy, dt)
+    pp = pCorrect(newRhoU1, newRhoU2, ps, dx, dy, dt)
+
+    newP = ps + pp
 
     return newRhoU1, newRhoU2, newP
 
@@ -152,10 +158,10 @@ def postProgress(u1, y, tStepNumb, totalt):
 
 def main():
 
-    x, y, u1, u2, ps, pp, nt, mu, Courant = init()
+    x, y, u1, u2, rhoU1, rhoU2, ps, pp, nt, mu, Courant = init()
     for t in range(nt):
 
-        newRhoU1, newRhoU2, newP = tscheme(x, y, rhoU1, rhoU2, ps, pp, mu, Courant)
+        newRhoU1, newRhoU2, newP = tscheme(x, y, u1, u2, rhoU1, rhoU2, ps, pp, mu, Courant)
 
         rhoU1 = newRhoU1
         rhoU2 = newRhoU2
