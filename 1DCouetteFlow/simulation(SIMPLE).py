36a610bdf4a6acb2a8f3afe1cf2f06f59746f58c
ChenHan <chenhan200006@shu.edu.cn>
2024-09-12 12:57:44 +0800

    modified CouetteFlow(SIMPLE).py

--- 1DCouetteFlow/simulation(SIMPLE).py
+++ 1DCouetteFlow/simulation(SIMPLE).py
@@ -19,16 +19,19 @@ def checkDiv(u1, u2, dx, dy):
     maxDiv = np.max(div)
     return maxDiv
 
-def velBCSet(u1, u2, U):
-    # inlet BC
-    u2[0,:] = 0
+def velBCSet(u1, u2, U, outu2):
     # upper BC
     u1[:,-1] = U
     u2[:,-1] = 0
     # lower BC
     u1[:,0] = 0
     u2[:,0] = 0
-    return u1, u2
+    # inlet
+    u1[0,1:-1] = u1[1,1:-1]
+    # outlet
+    outu2[:] = u2[-1,:]
+    u1[-1,1:-1] = u1[-2,1:-1]
+    return u1, u2, outu2
 
 def presBCSet(pp):
     # inlet
@@ -52,44 +55,43 @@ def init():
         y = np.linspace(0,Ly, Ny)
         U = 1 #ft/s
 
-        Courant = 0.5
         rho = 0.002377 #slug/ft^3
         mu = U*Ly*rho/63.6
-        nt = 100
 
-        return x, y, U, mu, rho, nt, Courant
+        return x, y, U, mu, rho
 
     def originalVariablesInit(x, y):
         p = np.zeros((len(x),len(y)),dtype=float)
         u1 = np.zeros((len(x)+1,len(y)),dtype=float)
         u2 = np.zeros((len(x),len(y)+1),dtype=float)
-        inu2 = np.zeros((1,len(y)+1),dtype=float)   #Ghost Cell
-        outu2 = np.zeros((1,len(y)+1),dtype=float)   #Ghost Cell
+        inu2 = np.zeros(len(y)+1,dtype=float)   # Ghost Cell:inlet BC
+        outu2 = np.zeros(len(y)+1,dtype=float)   #Ghost Cell:outlet BC
         return u1, u2, p, inu2, outu2
 
-    def flowFieldInit(u1, u2, U, pstar):
+    def flowFieldInit(u1, u2, U, pss):
         u1[:,:] = 0
         u2[:,:] = 0
-        pstar[:,:] = 0
-        pp = np.zeros_like(pstar)
+        ps[:,:] = 0
+        pp = np.zeros_like(ps)
         u1[14,4] = 0.5 #ft/s
-        u1, u2 = velBCSet(u1, u2, U)
 
-        return u1, u2, pstar, pp
+        return u1, u2, ps, pp
 
-    x ,y ,U, mu, rho, nt, Courant = parameterInput()
+    x ,y ,U, mu, rho = parameterInput()
     u1, u2, ps, inu2, outu2 = originalVariablesInit(x, y)
     u1, u2, ps, pp = flowFieldInit(u1, u2, U, ps)
+    u1, u2, outu2 = velBCSet(u1, u2, U, outu2)
     pp = presBCSet(pp)
 
-    return x, y, u1, u2, inu2, outu2, ps, pp, rho, nt, mu, Courant, U
+    return x, y, u1, u2, inu2, outu2, ps, pp, rho, mu, U
 
-def tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, Courant, U):
+def tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, U):
 
     def semiStep(u1, u2, inu2, outu2, ps, rho, dx, dy, dt, mu):
         # calculate velocity with divergence
         newRhoU1, newRhoU2 = np.zeros_like(u1), np.zeros_like(u2)
-
+        inu2[:] = 0
+        outu2[:] = u2[-1,:]
         for i in range(1, len(u1[:,0])-1):
             for j in range(1, len(u1[0,:])-1):
                 pI = i - 1
@@ -101,16 +103,29 @@ def tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, Courant, U):
                              (rho*u1[i,j+1]*(u2[iu2,ju2+1]+u2[iu2+1,ju2+1]) - rho*u1[i,j-1]*(u2[iu2,ju2-1]+u2[iu2+1,ju2-1]))/2/2/dy)
                 newRhoU1[i,j] = rho*u1[i,j] + dt*(-presTerm + convTerm + visTerm)
 
-        for i in range(1, len(u2[:,0])-1):
-            for j in range(1, len(u2[0,:])-1):
+        for j in range(1, len(u2[0,:])-1):
+            for i in range(len(u2[:,0])):
                 pJ = j - 1
                 iu1 = i + 1
                 ju1 = j - 1
-                presTerm = (ps[i,pJ+1] - ps[i,pJ+1])/dy
-                visTerm = mu*((u2[i+1,j]-2*u2[i,j]+u2[i-1,j])/dx**2 + (u2[i,j+1]-2*u2[i,j]+u2[i,j-1])/dy**2)
-                convTerm = -((rho*u2[i+1,j]*(u1[iu1+1,ju1]+u1[iu1+1,ju1+1]) - rho*u2[i-1,j]*(u1[iu1-1,ju1]+u1[iu1-1,ju1+1]))/2/2/dx+
-                             (rho*u2[i,j+1]**2 - rho*u2[i,j-1]**2)/2/dy)
-                newRhoU2[i,j] = rho*u2[i,j] + dt*(-presTerm + convTerm + visTerm)
+                if i == 0:
+                    presTerm = (ps[i,pJ+1] - ps[i,pJ+1])/dy
+                    visTerm = mu*((u2[i+1,j]-2*u2[i,j]+inu2[j])/dx**2 + (u2[i,j+1]-2*u2[i,j]+u2[i,j-1])/dy**2)
+                    convTerm = -((rho*u2[i+1,j]*(u1[iu1+1,ju1]+u1[iu1+1,ju1+1]) - rho*inu2[j]*(u1[iu1-1,ju1]+u1[iu1-1,ju1+1]))/2/2/dx+
+                                 (rho*u2[i,j+1]**2 - rho*u2[i,j-1]**2)/2/dy)
+                    newRhoU2[i,j] = rho*u2[i,j] + dt*(-presTerm + convTerm + visTerm)
+                elif i == len(u2[:,0])-1:
+                    presTerm = (ps[i,pJ+1] - ps[i,pJ+1])/dy
+                    visTerm = mu*((outu2[j]-2*u2[i,j]+u2[i-1,j])/dx**2 + (u2[i,j+1]-2*u2[i,j]+u2[i,j-1])/dy**2)
+                    convTerm = -((rho*outu2[j]*(u1[iu1,ju1]+u1[iu1,ju1+1]) - rho*u2[i-1,j]*(u1[iu1-1,ju1]+u1[iu1-1,ju1+1]))/2/2/dx+ #u1[iu1+1,ju1] = u1[iu1,ju1]
+                                 (rho*u2[i,j+1]**2 - rho*u2[i,j-1]**2)/2/dy)
+                    newRhoU2[i,j] = rho*u2[i,j] + dt*(-presTerm + convTerm + visTerm)
+                else:
+                    presTerm = (ps[i,pJ+1] - ps[i,pJ+1])/dy
+                    visTerm = mu*((u2[i+1,j]-2*u2[i,j]+u2[i-1,j])/dx**2 + (u2[i,j+1]-2*u2[i,j]+u2[i,j-1])/dy**2)
+                    convTerm = -((rho*u2[i+1,j]*(u1[iu1+1,ju1]+u1[iu1+1,ju1+1]) - rho*u2[i-1,j]*(u1[iu1-1,ju1]+u1[iu1-1,ju1+1]))/2/2/dx+
+                                 (rho*u2[i,j+1]**2 - rho*u2[i,j-1]**2)/2/dy)
+                    newRhoU2[i,j] = rho*u2[i,j] + dt*(-presTerm + convTerm + visTerm)
 
         return  newRhoU1, newRhoU2
 
@@ -119,14 +134,16 @@ def tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, Courant, U):
         a = 2*(1/dx**2 + 1/dy**2)*dt
         b = -dt/dx**2
         c = -dt/dy**2
+        newpp = np.zeros_like(pp)
         # solve Poisson equation using Relaxation Method
         for i in range(1,len(pp[:,0])-1):
             for j in range(1,len(pp[0,:])-1):
                 # iu1 = i + 1
                 # ju2 = j + 1
                 d = (rhoU1[i+1,j] - rhoU1[i,j])/dx + (rhoU2[i,j+1] - rhoU2[i,j])/dy
-                pp[i,j] = (b*(pp[i+1,j] + pp[i-1,j]) + c*(pp[i,j+1] + pp[i,j-1]) + d)/a
-        return pp
+                newpp[i,j] = -(b*(pp[i+1,j] + pp[i-1,j]) + c*(pp[i,j+1] + pp[i,j-1]) + d)/a
+        newpp = presBCSet(newpp)
+        return newpp
 
     def uCorrect(semiRhoU1, semiRhoU2, pp, dx, dy, dt):
 
@@ -147,14 +164,28 @@ def tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, Courant, U):
     dt = 0.001#Courant*np.sqrt(dx**2 + dy**2)
 
     semiRhoU1, semiRhoU2 = semiStep(u1, u2, inu2, outu2, ps, rho, dx, dy, dt, mu)
-    semiU1, semiU2 = velBCSet(semiRhoU1[:,:]/rho, semiRhoU2[:,:]/rho, U)
+    semiU1, semiU2, outu2 = velBCSet(semiRhoU1[:,:]/rho, semiRhoU2[:,:]/rho, U, outu2)
     semiRhoU1, semiRhoU2 = semiU1*rho, semiU2*rho
-    pp = pCorrect(semiRhoU1, semiRhoU2, pp, dx, dy, dt)
-    pp = presBCSet(pp)
 
-    newP = ps + pp
+    # iterNum = 0
+    # while(1):
+    #     pp = pCorrect(semiRhoU1, semiRhoU2, pp, dx, dy, dt)
+    #     if np.max(pp) <= 1e-3:
+    #         newP = ps + pp
+    #         print("iteration times of Poisson's Equation: ", iterNum)
+    #         break
+    #     iterNum += 1
+
+    for i in range(200):
+        newpp = pCorrect(semiRhoU1, semiRhoU2, pp, dx, dy, dt)
+        pp = newpp
+        if i == 199:
+            print(np.max(pp-newpp))
+
+    newP = ps + 0.1*pp
+
     newRhoU1, newRhoU2 = uCorrect(semiRhoU1, semiRhoU2, pp, dx, dy, dt)
-    newU1, newU2 = velBCSet(newRhoU1/rho, newRhoU2/rho, U)
+    newU1, newU2, outu2 = velBCSet(newRhoU1/rho, newRhoU2/rho, U, outu2)
 
     return newU1, newU2, newP, dx, dy
 
@@ -190,10 +221,10 @@ def postProgress(u1, y, tStepNumb, totalt):
 
 def main():
 
-    x, y, u1, u2, inu2, outu2, ps, pp, rho, nt, mu, Courant, U = init()
+    x, y, u1, u2, inu2, outu2, ps, pp, rho, mu, U = init()
     t = 0
     while(1):#for t in range(nt):
-        newU1, newU2, newP, dx, dy = tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, Courant, U)
+        newU1, newU2, newP, dx, dy = tscheme(x, y, u1, u2, inu2, outu2, rho, ps, pp, mu, U)
 
         u1 = newU1
         u2 = newU2
@@ -203,9 +234,8 @@ def main():
         if checkDiv(u1,u2,dx,dy) <= 1e-5:
             break
 
-        if t == 150:
+        if t == 300:
             print("flag")
-            break
         if t == 1000:
             print("maximum iteration times")
             break
